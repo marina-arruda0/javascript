fdcaa652e43e8deea79842572b6310311be11d33
marina-arruda0 <marina.arruda.valerio2@gmail.com>
2026-09-15 14:40:18 -0400

    add scrita

---
+++ op-comparacao-de-igualdade-escrita-arrays.js
@@ -0,0 +1,10 @@
+// listaA é um array com 5 elementos
+let listaA = [10, 20, 30, 40, 50];
+
+// listaB é um array com os mesmo 5 elementos que a lestaA
+let listaB = [10, 20, 30, 40, 50];
+
+//lstaC recebe uma copia da listaA
+let listaC = listaA;
+
+console.log(listaA === listaC); 
