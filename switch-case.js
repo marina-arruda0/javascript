6f25d0e4605ebbfbf530a019376c18fe74f28e45
marina-arruda0 <marina.arruda.valerio2@gmail.com>
2026-09-22 14:41:58 -0400

    add swith case

---
+++ switch-case.js
@@ -0,0 +1,27 @@
+const classe = "Mago";
+switch (classe) {
+case "Guerreiro":
+console. log("CLasse: Guerreiro");
+console. log("O guerreiro é um combatente corpo a corpo, especializado em força e resistência."):
+console. Log("Habilidades: Ataque poderoso, Defesa sólida, Fúria de batalha");
+break;
+case "Mago":
+console. Log("CLasse: Mago");
+console. Log("O mago é um conjurador de feitiços, especializado em magia ofensiva e suporte.");
+console. Log("Habilidades: Magia de fogo, Magia de gelo, Magia de cura");
+break;
+case
+"Arqueiro":
+console. Log("CLasse: Arqueiro");
+console. Log("O arqueiro é um combatente à distância, especializado em precisão e agilidade.");
+console. Log("Habilidades: Precisão, Agilidade, Tiro certero");
+break;
+case
+"Ladino":
+console. Log("Classe: Ladino");
+console. Log("O Ladino é um especialista em furtividade e trapas, utilizado para sabotagen e espionagen.");
+console. Log("Habilidades: Furtividade, Trapas, Assassínio");
+break;
+default:
+console. Log ("CLasse desconhecida. Por favor, escolha entre guerreiro, mago, arqueiro ou ladino. ");
+}
