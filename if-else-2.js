d1d3000a9ec23ee5aa9fd4b1da476da6f0c42fdc
marina-arruda0 <marina.arruda.valerio2@gmail.com>
2026-09-22 15:24:07 -0300

    add if else 2

---
+++ if-else-2.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "quarta-feira";
+
+if (diaDasemana === "sábado" || diaDaSemana === "domingo") {
+    console.log("É fim de semana!");
+} else {
+        console.log("É dia de semana.")
+    }
