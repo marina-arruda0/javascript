8949b038caf1b71f402ea3eacd3512d4002558dd
marina-arruda0 <marina.arruda.valerio2@gmail.com>
2026-09-22 15:24:14 -0300

    add ifjs

---
+++ if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12){
+    console.log("Bom dia!");
+}
+// O restante do código continua independente se a condição for verdadeira ou não.
+consolole.log("Dinheiro não traz felicidade. Me dê seu e seja feliz");
